7a49d32edc4aee47a13785862745e544bf23b403
sts <levpugin07@gmail.com>
2023-04-26 18:30:04 +0300

    right or left

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-int main (){
- int a = 5;
- std::cout << a;
- std::cout << " a != 0";
- 
+
+
+enum field_side {
+    right,
+    left
+};
+bool check_hit(int field_width, int ball_x) {
+
+    if (ball_x == 1 || ball_x == field_width)
+        return true;
+    return false;
 }
+
+int left_or_right(int field_width, int ball_x) {
+    field_side;
+    if (ball_x > field_width / 2) {
+        return right;
+    }
+    return left;
+}
+
